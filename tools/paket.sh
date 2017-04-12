43cdf2baa008368b8464d9820c31066b9e26c97b
ivaylo5ev <ivaylo5ev@gmail.com>
2017-04-12 18:15:03 +0300

    paket shell script minor improvements

--- tools/paket.sh
+++ tools/paket.sh
@@ -1,15 +1,15 @@
 PAKET='${0%/*}/../.paket/paket.exe'
-CALLEXE=''
+EXEC=''
 if [ "$(uname)" == "Darwin" ]; then
-  CALLEXE="mono ";
+  EXEC="mono ";
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  CALLEXE="mono ";
+  EXEC="mono ";
 #elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
   # Do something under 32 bits Windows NT platform
 #elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
   # Do something under 64 bits Windows NT platform
 fi
 if [ ! -f $PAKET ]; then
-  eval "$CALLEXE${PAKET%.exe}.bootstrapper.exe"
+  eval "$EXEC${PAKET%.exe}.bootstrapper.exe"
 fi
-eval "$CALLEXE$PAKET \"$@\""
+eval "$EXEC$PAKET \"$@\""
