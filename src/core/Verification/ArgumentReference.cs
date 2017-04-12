8386ad166e87e00a0bdcf8dc14fde45225ed05af
ivaylo5ev <ivaylo5ev@gmail.com>
2017-04-12 17:20:19 +0300

    doc comments for the ArgumentReference`1 class

--- src/core/Verification/ArgumentReference.cs
+++ src/core/Verification/ArgumentReference.cs
@@ -6,6 +6,10 @@ using System.Diagnostics;
 
 namespace Axle.Verification
 {
+    /// <summary>
+    /// A struct that represents a reference to an argument for a method or constructor. 
+    /// The argument reference is usually represented by its name (as defined in the respective method/constructor) and the value passed to it. 
+    /// </summary>
     public struct ArgumentReference<T>// : IReference<T>
     {
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
@@ -38,11 +42,31 @@ namespace Axle.Verification
             return this;
         }
 
+        /// <summary>
+        /// Determines whether the argument represented by this <see cref="ArgumentReference{T}"/> instance is of the type specified by 
+        /// the <paramref name="expectedType"/> parameter.
+        /// </summary>
+        /// <param name="expectedType">
+        /// The expected type for the argument represented by the current <see cref="ArgumentReference{T}"/> instance. 
+        /// </param>
+        /// <returns>
+        /// The <see cref="ArgumentReference{T}"/> instance that represents the verified argument.
+        /// </returns>
         [DebuggerStepThrough]
         public ArgumentReference<T> IsOfType(Type expectedType)
         {
             return IsOfTypeUnchecked(expectedType.VerifyArgument("expectedType").IsNotNull());
         }
+        /// <summary>
+        /// Determines whether the argument represented by this <see cref="ArgumentReference{T}"/> instance is of the type specified by 
+        /// the <typeparamref name="TExpected"/> generic parameter.
+        /// </summary>
+        /// <typeparam name="TExpected">
+        /// The expected type for the argument represented by the current <see cref="ArgumentReference{T}"/> instance. 
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="ArgumentReference{T}"/> instance that represents the verified argument.
+        /// </returns>
         [DebuggerStepThrough]
         public ArgumentReference<TExpected> IsOfType<TExpected>()
         {
@@ -52,10 +76,16 @@ namespace Axle.Verification
         }
 
         internal string Name { get { return name; } }
+        /// <summary>
+        /// Gets the value passed in the argument the current <see cref="ArgumentReference{T}"/> instance represents.
+        /// </summary>
         public T Value { get { return value; } }
         //T IReference<T>.Value { get { return value; } }
         //object IReference.Value { get { return value; } }
 
+        /// <param name="reference">
+        /// The argument reference instance to be unwrapped by this operator. 
+        /// </param>
         public static implicit operator T(ArgumentReference<T> reference) { return reference.value; }
     }
 }
