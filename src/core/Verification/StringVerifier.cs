20df087d05ec461d35b40b59712863878c29f8ed
ivaylo5ev <ivaylo5ev@gmail.com>
2017-04-12 14:26:25 +0300

    StringVerifier doc comments

--- src/core/Verification/StringVerifier.cs
+++ src/core/Verification/StringVerifier.cs
@@ -11,25 +11,63 @@ namespace Axle.Verification
     /// <seealso cref="string"/>
     public static class StringVerifier
     {
-        #if net45
-        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
-        #endif
-        [DebuggerStepThrough]
-        public static ArgumentReference<string> IsNotEmpty(this ArgumentReference<string> argument, string message)
+        private static ArgumentReference<string> UncheckedIsNotEmpty(ArgumentReference<string> argument, string message)
         {
-            if (Verifier.IsNotNull(argument).Value.Length == 0)
+            if (argument.Value.Length == 0)
             {
                 throw new ArgumentException(message ?? string.Format("Argument `{0}` cannot be an empty string.", argument.Name), argument.Name);
             }
             return argument;
         }
 
+        /// <summary>
+        /// Ensures the <see cref="ArgumentReference{T}">argument reference</see> represented by the <paramref name="argument"/>
+        /// is a non-empty string. 
+        /// </summary>
+        /// <param name="argument">
+        /// The argument that is being verified.
+        /// </param>
+        /// <param name="message">
+        /// An optional exception message to be used for the generated exception in case the argument is an empty string.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ArgumentReference{T}"/> instance that represents the verified argument.
+        /// </returns>
+        #if net45
+        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+        #endif
+        [DebuggerStepThrough]
+        public static ArgumentReference<string> IsNotEmpty(this ArgumentReference<string> argument, string message)
+        {
+            return UncheckedIsNotEmpty(Verifier.IsNotNull(argument), message);
+        }
+
+        /// <summary>
+        /// Ensures the <see cref="ArgumentReference{T}">argument reference</see> represented by the <paramref name="argument"/>
+        /// is a non-empty string. 
+        /// </summary>
+        /// <param name="argument">
+        /// The argument that is being verified.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ArgumentReference{T}"/> instance that represents the verified argument.
+        /// </returns>
         #if net45
         [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
         #endif
         [DebuggerStepThrough]
         public static ArgumentReference<string> IsNotEmpty(this ArgumentReference<string> argument) { return IsNotEmpty(argument, null); }
 
+        /// <summary>
+        /// Ensures the <see cref="ArgumentReference{T}">argument reference</see> represented by the <paramref name="argument"/>
+        /// is not <c>null</c> or an empty string. 
+        /// </summary>
+        /// <param name="argument">
+        /// The argument that is being verified.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ArgumentReference{T}"/> instance that represents the verified argument.
+        /// </returns>
         #if net45
         [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
         #endif
@@ -39,13 +77,32 @@ namespace Axle.Verification
             return IsNotEmpty(Verifier.IsNotNull(argument));
         }
 
+        /// <summary>
+        /// Ensures the <see cref="ArgumentReference{T}">argument reference</see> represented by the <paramref name="argument"/>
+        /// is not <c>null</c> or an empty string. 
+        /// </summary>
+        /// <param name="argument">
+        /// The argument that is being verified.
+        /// </param>
+        /// <param name="nullMessage">
+        /// An optional exception message to be used for the generated exception in case the argument is <c>null</c>.
+        /// </param>
+        /// <param name="emptyMessage">
+        /// An optional exception message to be used for the generated exception in case the argument is an empty string.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ArgumentReference{T}"/> instance that represents the verified argument.
+        /// </returns>
         #if net45
         [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
         #endif
         [DebuggerStepThrough]
-        public static ArgumentReference<string> IsNotNullOrEmpty(this ArgumentReference<string> argument, string emptyMessage)
+        public static ArgumentReference<string> IsNotNullOrEmpty(
+            this ArgumentReference<string> argument, 
+            string nullMessage, 
+            string emptyMessage)
         {
-            return IsNotEmpty(Verifier.IsNotNull(argument), emptyMessage);
+            return UncheckedIsNotEmpty(Verifier.IsNotNull(argument, nullMessage), emptyMessage);
         }
     }
 }
