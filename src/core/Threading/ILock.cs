1510c29d65250fd629f446d65386b4ffcd020ad3
ivaylo5ev <ivaylo5ev@gmail.com>
2017-04-13 22:26:09 +0300

    added ILock interface

---
+++ src/core/Threading/ILock.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Axle.Threading
+{
+    /// <summary>
+    /// An interface representing a synchronization lock construct.
+    /// </summary>
+    public interface ILock
+    {
+        /// <summary>
+        /// Acquires an exclusive lock. 
+        /// </summary>
+        void Enter();
+
+        /// <summary>
+        /// Releases a previously obtained exclusive lock. 
+        /// </summary>
+        void Exit();
+
+        /// <summary>
+        /// Attempts to acquire an exclusive lock. 
+        /// </summary>
+        /// <param name="millisecondsTimeout">
+        /// The number of milliseconds to wait for the lock. 
+        /// A value of <c>-1</c> represents an infinite wait. 
+        /// </param>
+        /// <seealso cref="System.Threading.Timeout.Infinite"/>
+        bool TryEnter(int millisecondsTimeout);
+        /// <summary>
+        /// Attempts to acquire an exclusive lock. 
+        /// </summary>
+        /// <param name="timeout">
+        /// A <see cref="TimeSpan"/> representing the amount of time to wait for the lock. 
+        /// </param>
+        bool TryEnter(TimeSpan timeout);
+    }
+}
