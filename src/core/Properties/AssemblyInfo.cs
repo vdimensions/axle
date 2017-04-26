ce9c2aecec1ec72bdf95300d7b4943e7dfdd2cb6
ivaylo5ev <ivaylo5ev@gmail.com>
2017-04-27 01:49:21 +0300

    -removed System.Configuration assembly reference from Axle.Core nuspec

--- src/core/Properties/AssemblyInfo.cs
+++ src/core/Properties/AssemblyInfo.cs
@@ -16,6 +16,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("43919C97-BCFD-4802-95D7-76435E066962")]
 
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
-[assembly: AssemblyInformationalVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.5.0.1")]
+[assembly: AssemblyFileVersion("1.5.0.1")]
+[assembly: AssemblyInformationalVersion("1.5.0.1")]
