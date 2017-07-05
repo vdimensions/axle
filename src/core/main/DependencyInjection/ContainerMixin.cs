a68e93333976f4d3e5152fd35a36a940a00e1fdf
ivaylo5ev <ivaylo5ev@gmail.com>
2017-07-05 11:14:21 +0300

    - basic methods for the IContainer interface and its mixin

---
+++ src/core/main/DependencyInjection/ContainerMixin.cs
@@ -0,0 +1,24 @@
+using System;
+
+using Axle.Verification;
+
+
+namespace Axle.DependencyInjection
+{
+    public static class ContainerMixin
+    {
+        public static object Resolve(this IContainer container, Type type)
+        {
+            return container.VerifyArgument(nameof(container)).IsNotNull().Value.Resolve(type, string.Empty);
+        }
+
+        public static T Resolve<T>(this IContainer container, string name)
+        {
+            return (T) container.VerifyArgument(nameof(container)).IsNotNull().Value.Resolve(typeof(T));
+        }
+        public static T Resolve<T>(this IContainer container)
+        {
+            return Resolve<T>(container, string.Empty);
+        }
+    }
+}
--- src/core/main/DependencyInjection/IContainer.cs
+++ src/core/main/DependencyInjection/IContainer.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+
 namespace Axle.DependencyInjection
 {
+    /// <summary>
+    /// An interface representing a dependency container.
+    /// </summary>
     public interface IContainer
     {
-    }
-
-    public abstract class AbstractContainer : IContainer
-    {
-        
+        object Resolve(Type type, string name);
     }
 }
