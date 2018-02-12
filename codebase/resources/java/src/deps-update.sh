1363604323976edf554b7ce3d8c34b6aca04bd84
ivaylo5ev <ivaylo5ev@gmail.com>
2018-02-12 15:41:45 +0200

    * added net35-compatible version of Kajabity.Tools.Java * updated Axle.Resources.Java to reference the Kajabity.Tools.Java project

---
+++ codebase/resources/java/src/deps-update.sh
@@ -0,0 +1,15 @@
+project='Axle.Resources.Java'
+
+./paket.sh update
+if [ $? -ne 0 ]; then
+  read -rsp "Press [Enter] to quit"
+  echo ""
+  exit
+fi
+dotnet restore $project.csproj
+if [ $? -ne 0 ]; then
+  read -rsp "Press [Enter] to quit"
+  echo ""
+  exit
+fi
+
