4d5c11ee264d14ad5c4bdfbcb4dfe03ff36ce83c
ivaylo5ev <ivaylo5ev@gmail.com>
2018-11-01 18:22:55 +0200

    * application: added new build for Axle.Forest that has a manifest embedded

--- codebase/application/forest/src/build.sh
+++ codebase/application/forest/src/build.sh
@@ -1,14 +1,15 @@
+msbuild="../../../../submodules/btw/msbuild.sh"
 project='Axle.Forest'
 
 ./restore.sh
 
-dotnet msbuild $project.fsproj
+$msbuild $project.fsproj
 if [ $? -ne 0 ]; then
   read -rsp "Press [Enter] to quit"
   echo ""
   exit
 fi
-dotnet msbuild $project.dist.csproj
+$msbuild $project.dist.csproj
 if [ $? -ne 0 ]; then
   read -rsp "Press [Enter] to quit"
   echo ""
