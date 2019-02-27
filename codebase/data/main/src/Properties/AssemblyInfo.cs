5a0f3e3d157b945bf0b1c0099025e8326338d266
ivaylo5ev <ivaylo5ev@gmail.com>
2019-02-27 15:23:31 +0200

    * data: removed mason dependencies and legacy msbuild references from Axle.Data project

--- codebase/data/main/src/Properties/AssemblyInfo.cs
+++ codebase/data/main/src/Properties/AssemblyInfo.cs
@@ -2,22 +2,8 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Axle.Data")]
-[assembly: AssemblyProduct("Axle.Data")]
-[assembly: AssemblyDescription("Axle framework's data access library")]
-
-[assembly: AssemblyCompany("Virtual Dimensions")]
-[assembly: AssemblyCopyright("Copyright © Virtual Dimensions 2013-2018")]
-[assembly: AssemblyTrademark("")]
-
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
 #if NETFRAMEWORK || NETSTANDARD1_1_OR_NEWER
 [assembly: Guid("1DA50E5B-0778-4E5D-8BF5-08AE4C645647")]
 #endif
 
-[assembly: AssemblyVersion("1.5.1.2")]
-[assembly: AssemblyFileVersion("1.5.1.2")]
-[assembly: AssemblyInformationalVersion("1.5.1.2")]
