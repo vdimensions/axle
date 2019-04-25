f11cd4519df2d6d26fa30dc099e3fa515bc84270
ivaylo5ev <ivaylo5ev@gmail.com>
2019-04-25 14:58:54 +0300

    * docs: docgen script changes

--- docs/docgen.sh
+++ docs/docgen.sh
@@ -14,11 +14,15 @@ docfx
 cd ../
 
 echo "Committing generated documentation in brach 'docgen'"
-git add $docsDir && git commit -m "Pushing docfx generated documentation"
+git add $docsDir -f && git commit -m "Pushing docfx generated documentation"
 #git subtree push --prefix $docsDir $remote gh-pages
 
-echo "Pushing subtree with documentation from $remote:docgen into $remote:gh-pages branch"
-git push $remote `git subtree split --prefix $docsDir docgen 2> /dev/null`:gh-pages --force
+#echo "Pushing subtree with documentation from $remote:docgen into $remote:gh-pages branch"
+#git push $remote `git subtree split --prefix $docsDir docgen 2> /dev/null`:gh-pages --force
+git branch -D gh-pages
+git subtree push --prefix $docsDir gh-pages && git checkout gh-pages
+#git branch gh-pages && git checkout gh-pages
+git push $remote gh-pages --force
 
 echo "Restoring current branch $currentBranch"
 git checkout $currentBranch
