5d72bb53cef3648d7a5eef9ff076e6ff1cbadb77
ivaylo5ev <ivaylo5ev@gmail.com>
2019-05-20 00:55:00 +0300

    * data: mysql data access library initial commit

---
+++ codebase/data/mysql/src/build.sh
@@ -0,0 +1,12 @@
+paket='.paket/paket.sh'
+project='Axle.Data.MySql'
+project_format='csproj'
+
+./restore.sh
+
+dotnet clean $project.$project_format && dotnet build $project.$project_format && dotnet pack $project.$project_format --no-build --no-restore
+if [ $? -ne 0 ]; then
+  read -rsp "Press [Enter] to quit"
+  echo ""
+  exit
+fi
