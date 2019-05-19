2ca2bec3352593b57a0c67865e2ef72b3a23fe72
ivaylo5ev <ivaylo5ev@gmail.com>
2019-05-20 01:14:23 +0300

    * data: OLE DB library brought up to date

--- codebase/data/oledb/src/Properties/AssemblyInfo.cs
+++ codebase/data/oledb/src/Properties/AssemblyInfo.cs
@@ -1,23 +1,6 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Axle.Data.OleDb")]
-[assembly: AssemblyProduct("Axle.Data.OleDb")]
-[assembly: AssemblyDescription("Axle framework's OleDB data module")]
-
-[assembly: AssemblyCompany("Virtual Dimensions")]
-[assembly: AssemblyCopyright("Copyright © Virtual Dimensions 2013-2018")]
-[assembly: AssemblyTrademark("")]
-
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCulture("")]
+﻿using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 #if NETFRAMEWORK || NETSTANDARD1_1_OR_NEWER
 [assembly: Guid("3CFAC4A9-56FE-4036-ADB2-122F61C564EB")]
 #endif
-
-[assembly: AssemblyVersion("1.5.1.1")]
-[assembly: AssemblyFileVersion("1.5.1.1")]
-[assembly: AssemblyInformationalVersion("1.5.1.1")]
