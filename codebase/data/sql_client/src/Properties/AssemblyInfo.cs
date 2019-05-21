af1b73f8010184cb192f7f0e293c9a8dd82c534f
ivaylo5ev <ivaylo5ev@gmail.com>
2019-05-21 15:07:52 +0300

    * data: brought MS SQL client library up to date

---
+++ codebase/data/sql_client/src/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+#if NETFRAMEWORK || NETSTANDARD1_1_OR_NEWER
+[assembly: Guid("82EE9E10-E6CE-485F-A943-BA33E303035E")]
+#endif
