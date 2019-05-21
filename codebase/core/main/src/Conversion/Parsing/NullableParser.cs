53d812fc3b384f0715f97d08e811f3462a208342
ivaylo5ev <ivaylo5ev@gmail.com>
2019-05-21 22:55:36 +0300

    * core: implemented nullable parser decorator, for easier parsing of nullable value types. `null` or zero-length strings will result in parsing the `null` equivalent of the nullable value type

---
+++ codebase/core/main/src/Conversion/Parsing/NullableParser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Axle.Verification;
+
+
+namespace Axle.Conversion.Parsing
+{
+    /// <summary>
+    /// A class that can parse <see cref="string">string</see> representations of a nullable
+    /// <typeparamref name="T"/> value.
+    /// </summary>
+    #if NETSTANDARD2_0_OR_NEWER || NETFRAMEWORK
+    [Serializable]
+    #endif
+    public sealed class NullableParser<T> : AbstractParser<T?> where T: struct
+    {
+        private readonly IParser<T> _actualParser;
+
+        public NullableParser(IParser<T> actualParser)
+        {
+            _actualParser = actualParser.VerifyArgument(nameof(actualParser)).IsNotNull().Value;
+        }
+
+        /// <inheritdoc />
+        protected override T? DoParse(string value, IFormatProvider formatProvider)
+        {
+            return string.IsNullOrEmpty(value)
+                ? null as T?
+                : _actualParser.Parse(value, formatProvider);
+        }
+
+        public override bool TryParse(string value, IFormatProvider formatProvider, out T? output)
+        {
+            output = null;
+            if (string.IsNullOrEmpty(value))
+            {
+                return true;
+            }
+
+            if (!_actualParser.TryParse(value, formatProvider, out var res))
+            {
+                return false;
+            }
+
+            output = res;
+            return true;
+        }
+    }
+}
---
+++ codebase/core/main/src/Conversion/Parsing/ParserExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Axle.Verification;
+
+
+namespace Axle.Conversion.Parsing
+{
+    /// <summary>
+    /// A static class holding extension methods for the <see cref="IParser{T}"/> type.
+    /// </summary>
+    public static class ParserExtensions
+    {
+        /// <summary>
+        /// Gets a <see cref="IParser{T?}"/> instance derived from the provided <paramref name="parser"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of values this parser can handle.</typeparam>
+        /// <param name="parser">
+        /// The underlying <see cref="IParser{T}"/> which is used to parse non-empty string representations of <typeparamref name="T"/>
+        /// </param>
+        /// <returns>
+        /// A <see cref="IParser{T?}"/> instance derived from the provided <paramref name="parser"/>.
+        /// </returns>
+        public static IParser<T?> GetNullableParser<T>(this IParser<T> parser) where T: struct
+        {
+            return new NullableParser<T>(parser.VerifyArgument(nameof(parser)).IsNotNull().Value);
+        }
+    }
+}
