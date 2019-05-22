7cae3f1e3533f62945744eaab3e3e531e553d8d5
ivaylo5ev <ivaylo5ev@gmail.com>
2019-05-22 13:00:09 +0300

    * core:  - updated unit tests to run via `dotnet test`  - added uri extension method tests

---
+++ codebase/core/main/tests/Extensions/UriExtensionsTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using Axle.Conversion.Parsing;
+using Axle.Extensions.Uri;
+using NUnit.Framework;
+using UriParser = Axle.Conversion.Parsing.UriParser;
+
+namespace Axle.Core.Tests.Extensions
+{
+    [TestFixture]
+    public class UriExtensionsTests
+    {
+        private IParser<Uri> _uriParser = new UriParser();
+
+        [Test]
+        public void TestUriResolveAppend()
+        {
+            var u1 = _uriParser.Parse("http://someuri.org");
+            var u2 = _uriParser.Parse("./home");
+            var resolved = u1.Resolve(u2);
+            Assert.AreEqual("http://someuri.org/home", resolved.ToString());
+        }
+        [Test]
+        public void TestUriResolveAppendNoSlash()
+        {
+            var u1 = _uriParser.Parse("http://someuri.org");
+            var u2 = _uriParser.Parse("home");
+            var resolved = u1.Resolve(u2);
+            Assert.AreEqual("http://someuri.org/home", resolved.ToString());
+        }
+
+        [Test]
+        public void TestUriResolveReplace()
+        {
+            var u1 = _uriParser.Parse("http://someuri.org/home");
+            var u2 = _uriParser.Parse("../about");
+            var resolved = u1.Resolve(u2);
+            Assert.AreEqual("http://someuri.org/about", resolved.ToString());
+        }
+
+
+        [Test]
+        public void TestUriResolveAbsolute()
+        {
+            var u1 = _uriParser.Parse("http://someuri.org/home");
+            var u2 = _uriParser.Parse("http://anotheruri.org/home");
+            var resolved = u1.Resolve(u2);
+            Assert.AreEqual("http://anotheruri.org/home", resolved.ToString());
+        }
+    }
+}
