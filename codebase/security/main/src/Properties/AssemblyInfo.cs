e72dca7d9e591d7c1f6f6d74554e8a87a72531e0
ivaylo5ev <ivaylo5ev@gmail.com>
2019-05-29 16:15:38 +0300

    * security: Upgraded Axle.Security project to work with dotnet cli

--- codebase/security/main/src/Properties/AssemblyInfo.cs
+++ codebase/security/main/src/Properties/AssemblyInfo.cs
@@ -1,23 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Axle.Security")]
-[assembly: AssemblyProduct("Axle.Security")]
-[assembly: AssemblyDescription("Axle framework's security module")]
-
-[assembly: AssemblyCompany("Virtual Dimensions")]
-[assembly: AssemblyCopyright("Copyright © Virtual Dimensions 2013-2018")]
-[assembly: AssemblyTrademark("")]
-
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 #if NETFRAMEWORK || NETSTANDARD1_1_OR_NEWER
 [assembly: Guid("7CDEE3ED-184E-497A-BD14-F949D544D52F")]
 #endif
-
-[assembly: AssemblyVersion("1.5.1.1")]
-[assembly: AssemblyFileVersion("1.5.1.1")]
-[assembly: AssemblyInformationalVersion("1.5.1.1")]
