a3e2b5b1c9a7132647fa2a2498fb55c68e4d60cb
ivaylo5ev <ivaylo5ev@gmail.com>
2019-07-16 15:54:57 +0300

    * configuration: json configuration support initial commit

---
+++ codebase/configuration/json/src/JsonConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Axle.Configuration.Json
+{
+    public sealed class JsonConfigurationReader : Axle.Configuration.IConfigurationReader
+    {
+        public IConfiguration LoadEmbeddedConfiguration(Assembly assembly, string resourceName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IConfiguration LoadEmbeddedConfiguration(Assembly assembly)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IConfiguration CurrentConfiguration { get; }
+    }
+
+    public class JsonConfiguration : IConfiguration
+    {
+        public IConfigurationSection GetSection(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetSection<T>() where T : class, IConfigurationSection
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetSection<T>(bool useDefault) where T : class, IConfigurationSection, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Path { get; }
+    }
+}
---
+++ codebase/configuration/json/src/build.sh
@@ -0,0 +1,11 @@
+project='Axle.Configuration.Json'
+project_format='csproj'
+
+./restore.sh
+
+dotnet clean $project.$project_format && dotnet build $project.$project_format && rm -rf ../../../../dist/$project.?.*.nupkg && dotnet pack $project.$project_format --no-build --no-restore
+if [ $? -ne 0 ]; then
+  read -rsp "Press [Enter] to quit"
+  echo ""
+  exit
+fi
