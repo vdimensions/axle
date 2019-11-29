ab5fd330df05aabf08f07804d2dfab0e9a0e6e33
ivaylo5ev <ivaylo5ev@gmail.com>
2019-11-29 13:20:26 +0200

    * async producer-consumer initial implementation

---
+++ codebase/core/main/src/Threading/AsyncProducer.cs
@@ -0,0 +1,115 @@
+﻿#if NETSTANDARD1_3_OR_NEWER || NET35_OR_NEWER
+using Axle.Verification;
+using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace Axle.Threading
+{
+    /// <summary>
+    /// A producer-consumer queue implementation that allows externalization
+    /// of the consumer logic.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of object representing the data being consumed.
+    /// </typeparam>
+    public class AsyncProducer<T> : IDisposable
+    {
+        private readonly BlockingCollection<T> _dataItems = new BlockingCollection<T>(1);
+        private readonly Task _workerTask;
+        private readonly IAsyncConsumer<T> _consumer;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="AsyncProducer{T}"/> class
+        /// using the specified <paramref name="consumer"/>.
+        /// </summary>
+        /// <param name="consumer">
+        /// A <see cref="IAsyncConsumer{T}"/> object to process
+        /// the data provided by the current <see cref="AsyncProducer{T}"/> instance.
+        /// </param>
+        public AsyncProducer(IAsyncConsumer<T> consumer)
+        {
+            _workerTask = StartWorker();
+            _consumer = consumer.VerifyArgument(nameof(consumer)).IsNotNull().Value;
+        }
+
+        /// <summary>
+        /// Finalizes the current <see cref="AsyncProducer{T}"/> instance.
+        /// </summary>
+        ~AsyncProducer() => Dispose(false);
+
+        private Task StartWorker()
+        {
+            #if NET35 || NET40
+            return Task.Factory.StartNew(() =>
+            #else
+            return Task.Run(() =>
+            #endif
+            {
+                while (!_dataItems.IsCompleted && !_wasDisposed)
+                {
+                    try
+                    {
+                        var data = _dataItems.Take();
+                        try
+                        {
+                            _consumer.Consume(data);
+                        }
+                        catch (Exception e)
+                        {
+                            _consumer.HandleError(e, data);
+                        }
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        // IOE means that Take() was called on a completed collection.
+                        // Some other thread can call CompleteAdding after we pass the
+                        // IsCompleted check but before we call Take. 
+                        // Here, we can simply catch the exception since the 
+                        // loop will break on the next iteration.
+                    }
+                }
+                _consumer.Complete();
+            });
+        }
+
+        /// <summary>
+        /// Adds an <paramref name="item"/> to the queue to be processed
+        /// by the consumer.
+        /// </summary>
+        /// <param name="item">
+        /// The item to be added for processing.
+        /// </param>
+        public void Push(T item) => _dataItems.Add(item);
+
+        #region IDisposable Support
+        private bool _wasDisposed = false;
+
+        /// <summary>
+        /// Disposes of the current <see cref="AsyncProducer{T}"/> instance.
+        /// </summary>
+        /// <param name="disposing">
+        /// A <see cref="bool"/> value indicating whether the dispose call is
+        /// being sent by a finalizer, or a manual dispose call.
+        /// </param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_wasDisposed)
+            {
+                return;
+            }
+            _wasDisposed = true;
+            if (disposing)
+            {
+                GC.SuppressFinalize(this);
+                _dataItems.CompleteAdding();
+                _workerTask.Wait();
+                (_workerTask as IDisposable)?.Dispose();
+            }
+        }
+
+        void IDisposable.Dispose() => Dispose(true);
+        #endregion
+    }
+}
+#endif
---
+++ codebase/core/main/src/Threading/IAsyncConsumer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Axle.Threading
+{
+    /// <summary>
+    /// An interface representing an async consumer. An async consumer is 
+    /// used in conjunction with an instance of <see cref="AsyncProducer{T}"/>
+    /// where the consumer's role is to process the data which is pushed to the
+    /// <see cref="AsyncProducer{T}"/> asyncronously.
+    /// <remarks>
+    /// The consumer itself works in syncrhonous context.
+    /// </remarks>
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of the consumed data.
+    /// </typeparam>
+    public interface IAsyncConsumer<T>
+    {
+        /// <summary>
+        /// Processed data suppied by an <see cref="AsyncProducer{T}"/>.
+        /// </summary>
+        /// <param name="data">
+        /// An instance of <typeparamref name="T"/> provided by an <see cref="AsyncProducer{T}"/>
+        /// </param>
+        void Consume(T data);
+
+        /// <summary>
+        /// A method invoked when an exception is being thrown while consuming.
+        /// </summary>
+        /// <param name="e">
+        /// The exception which occurred while processing data.
+        /// </param>
+        /// <param name="data">
+        /// The data item that was being processed while the exception was thrown.
+        /// </param>
+        void HandleError(Exception e, T data);
+
+        /// <summary>
+        /// A method invoked when the <see cref="AsyncProducer{T}"/> is no longer
+        /// receiving data.
+        /// </summary>
+        void Complete();
+    }
+}
