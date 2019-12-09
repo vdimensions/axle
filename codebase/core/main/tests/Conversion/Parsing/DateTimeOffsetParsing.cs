02d75cf27b9f0c04a913d45487f643eab5989612
ivaylo5ev <ivaylo5ev@gmail.com>
2019-12-09 20:36:03 +0200

    * aspnetcore: implemented support for static file hosting

--- codebase/core/main/tests/Conversion/Parsing/DateTimeOffsetParsing.cs
+++ codebase/core/main/tests/Conversion/Parsing/DateTimeOffsetParsing.cs
@@ -13,9 +13,41 @@ namespace Axle.Core.Tests.Conversion.Parsing
         [Test]
         public void TestDateTimeOffsetParsing()
         {
+            var date = DateTime.Now;
             var dateTimeOffset = new DateTimeOffset(
-                new DateTime(DateTime.Now.Ticks, DateTimeKind.Unspecified),
+                new DateTime(date.Ticks, DateTimeKind.Unspecified),
                 TimeSpan.FromHours(2));
+            var isoString = dateTimeOffset.ToISOString();
+            var parser = new DateTimeOffsetParser();
+
+            foreach (var culture in CultureInfo.GetCultures(CultureTypes.AllCultures))
+            {
+                var parsedOffset = culture.Parse(parser, isoString);
+                Assert.AreEqual(isoString, parsedOffset.ToISOString(), $"Failed parsing for culture {culture}");
+            }
+        }
+
+        [Test]
+        public void TestLocalDateTimeOffsetParsing()
+        {
+            var date = DateTime.Now;
+            var dateTimeOffset = new DateTimeOffset(
+                new DateTime(date.Ticks, DateTimeKind.Local), TimeSpan.FromHours(2));
+            var isoString = dateTimeOffset.ToISOString();
+            var parser = new DateTimeOffsetParser();
+
+            foreach (var culture in CultureInfo.GetCultures(CultureTypes.AllCultures))
+            {
+                var parsedOffset = culture.Parse(parser, isoString);
+                Assert.AreEqual(isoString, parsedOffset.ToISOString(), $"Failed parsing for culture {culture}");
+            }
+        }
+
+        [Test]
+        public void TestUtcDateTimeOffsetParsing()
+        {
+            var date = DateTime.UtcNow;
+            var dateTimeOffset = new DateTimeOffset(new DateTime(date.Ticks, DateTimeKind.Utc), TimeSpan.Zero);
             var isoString = dateTimeOffset.ToISOString();
             var parser = new DateTimeOffsetParser();
 
---
+++ codebase/web/main/src/StaticFiles/ApplicationBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+
+namespace Axle.Web.AspNetCore.StaticFiles
+{
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public static class ApplicationBuilderExtensions
+    {
+        [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+        public static IApplicationBuilder UseStaticFiles(this IApplicationBuilder app) => 
+            app.ConfigureModules(m => m.Load<StaticFilesModule>());
+    }
+}
