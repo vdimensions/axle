209c343b040079fd41728aac4866d4652827ad25
ivaylo5ev <ivaylo5ev@gmail.com>
2020-03-02 22:44:53 +0200

    fixed version numbers for dev branch

--- codebase/core/fsharp/src/build.sh
+++ codebase/core/fsharp/src/build.sh
@@ -1,10 +1,12 @@
 msbuild="../../../../submodules/btw/msbuild.sh"
-project='Axle.FSharp'
+project_name='Axle.FSharp'
+project_ext='.fsproj'
+project=${project_name}${project_ext}
 
 ./restore.sh
 
-dotnet clean $project.fsproj && $msbuild $project.fsproj && dotnet pack $project.fsproj --no-build
-if [ $? -ne 0 ]; then
+dotnet clean ${project} && ${msbuild} ${project} && dotnet pack ${project} --no-build
+if [[ $? -ne 0 ]]; then
   read -rsp "Press [Enter] to quit"
   echo ""
   exit
