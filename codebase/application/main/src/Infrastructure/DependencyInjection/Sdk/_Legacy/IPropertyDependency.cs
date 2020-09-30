cb2ed152ed93684285ec5ded58ae4b78e00d6c07
ivaylo5ev <ivaylo5ev@gmail.com>
2020-09-30 13:03:11 +0300

    - removed projects and files with features that are out of scope for the 1.10.x release

--- codebase/application/main/src/Infrastructure/DependencyInjection/Sdk/_Legacy/IPropertyDependency.cs
+++
@@ -1,9 +0,0 @@
-﻿//namespace Axle.Core.Infrastructure.DependencyInjection.Sdk
-//{
-//    public interface IPropertyDependency : IDependency
-//    {
-//        void Set(object target, object value);
-//
-//        string PropertyName { get; }
-//    }
-//}
