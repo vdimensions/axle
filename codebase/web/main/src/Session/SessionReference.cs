3a1f315ad4a550a7f6b4c9b2a1a56548edc7aa05
ivaylo5ev <ivaylo5ev@gmail.com>
2020-10-10 21:16:43 +0300

    - Axle.Web.AspNetCore changes

---
+++ codebase/web/main/src/Session/SessionReference.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using Axle.References;
+using Axle.Verification;
+using Microsoft.AspNetCore.Http;
+
+namespace Axle.Web.AspNetCore.Session
+{
+    public abstract class SessionReference<T> : IDisposable, IReference<T>
+    {
+        private readonly IHttpContextAccessor _accessor;
+        private readonly ConcurrentDictionary<string, T> _perSessionData = new ConcurrentDictionary<string, T>(StringComparer.Ordinal);
+
+        protected SessionReference(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+        
+        public void CompareReplace(T value, Func<T, T, T> compareFn)
+        {
+            compareFn.VerifyArgument(nameof(compareFn)).IsNotNull();
+            _perSessionData.AddOrUpdate(SessionId, value, (_, existing) => compareFn(existing, value));
+        }
+        
+        public  bool TryRemove(out T removed) => _perSessionData.TryRemove(SessionId, out removed);
+
+        public bool TryGetValue(out T value) => _perSessionData.TryGetValue(SessionId, out value);
+
+        public T Value
+        {
+            get => _perSessionData.TryGetValue(SessionId, out var result) ? result : default(T);
+            set => _perSessionData.AddOrUpdate(SessionId, value, (_, existing) => value);
+        }
+        object IReference.Value => Value;
+
+        public void Dispose()
+        {
+            var values = _perSessionData.Values.ToArray();
+            for (var i = 0; i < values.Length; i++)
+            {
+                var v = values[i];
+                if (v is IDisposable d)
+                {
+                    d.Dispose();
+                }
+            }
+            _perSessionData.Clear();
+        }
+        void IDisposable.Dispose() => this.Dispose();
+
+        public string SessionId => _accessor.HttpContext.Session.Id;
+    }
+}
