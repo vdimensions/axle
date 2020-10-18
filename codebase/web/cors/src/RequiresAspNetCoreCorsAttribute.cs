352ac22b3d82ad12a4f11f0498f7df452f406e71
ivaylo5ev <ivaylo5ev@gmail.com>
2020-10-18 14:35:36 +0300

    - fixed a bug with the `RequiresAspNetCoreCors` attribute acting like `UtilizesAspNetCoreCorsAttribute` due ti having the wring base class

--- codebase/web/cors/src/RequiresAspNetCoreCorsAttribute.cs
+++ codebase/web/cors/src/RequiresAspNetCoreCorsAttribute.cs
@@ -3,9 +3,16 @@ using Axle.Modularity;
 
 namespace Axle.Web.AspNetCore.Cors
 {
+    /// <summary>
+    /// An attribute that, when applied on a module class, will establish the <see cref="AspNetCoreCorsModule"/> as a
+    /// dependency on the target module.
+    /// </summary>
     [SuppressMessage("ReSharper", "UnusedMember.Global")]
-    public sealed class RequiresAspNetCoreCorsAttribute : UtilizesAttribute
+    public sealed class RequiresAspNetCoreCorsAttribute : RequiresAttribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RequiresAspNetCoreCorsAttribute"/> class.
+        /// </summary>
         public RequiresAspNetCoreCorsAttribute() : base(typeof(AspNetCoreCorsModule))
         {
         }
--- codebase/web/cors/src/UtilizesAspNetCoreCorsAttribute.cs
+++ codebase/web/cors/src/UtilizesAspNetCoreCorsAttribute.cs
@@ -6,6 +6,9 @@ namespace Axle.Web.AspNetCore.Cors
     [SuppressMessage("ReSharper", "UnusedMember.Global")]
     public sealed class UtilizesAspNetCoreCorsAttribute : UtilizesAttribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UtilizesAspNetCoreCorsAttribute"/> class.
+        /// </summary>
         public UtilizesAspNetCoreCorsAttribute() : base(typeof(AspNetCoreCorsModule))
         {
         }
