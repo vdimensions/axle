e3a3cfe2c398e587f40c7b12d578f12947df7f0c
ivaylo5ev <ivaylo5ev@gmail.com>
2020-12-17 14:25:07 +0200

    - separated logger wrappers into their own cs files

---
+++ codebase/logging/microsoft/src/Axle2MicrosoftLogger.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Logging;
+
+using System;
+
+namespace Axle.Logging.Microsoft
+{
+    using IMSLogger = global::Microsoft.Extensions.Logging.ILogger;
+
+    public sealed class Axle2MicrosoftLogger : IMSLogger
+    {
+        private readonly ILogger _logger;
+
+        public Axle2MicrosoftLogger(ILogger logger)
+        {
+            this._logger = logger;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            LogSeverity severity = LogSeverity.None;
+            switch (logLevel)
+            {
+                case LogLevel.Trace:
+                case LogLevel.Debug:
+                    severity = LogSeverity.Debug;
+                    break;
+                case LogLevel.Information:
+                    severity = LogSeverity.Info;
+                    break;
+                case LogLevel.Warning:
+                    severity = LogSeverity.Warning;
+                    break;
+                case LogLevel.Error:
+                    severity = LogSeverity.Error;
+                    break;
+                case LogLevel.Critical:
+                    severity = LogSeverity.Fatal;
+                    break;
+            }
+
+        }
+    }
+}
--- codebase/logging/microsoft/src/Axle2MicrosoftLoggerProvider.cs
+++ codebase/logging/microsoft/src/Axle2MicrosoftLoggerProvider.cs
@@ -1,20 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Axle.Logging.Microsoft
 {
     using IMSLogger = global::Microsoft.Extensions.Logging.ILogger;
     using IMSLoggerProvider = global::Microsoft.Extensions.Logging.ILoggerProvider;
-    using IMSLoggerFactory = global::Microsoft.Extensions.Logging.ILoggerFactory;
 
-    public sealed class Axle2MicrosoftLoggerFactory : IMSLoggerFactory
+    public sealed class Axle2MicrosoftLoggerProvider : IMSLoggerProvider
     {
-        public void AddProvider(IMSLoggerProvider provider)
-        {
-            throw new NotImplementedException();
-        }
-
         public IMSLogger CreateLogger(string categoryName)
         {
             throw new NotImplementedException();
