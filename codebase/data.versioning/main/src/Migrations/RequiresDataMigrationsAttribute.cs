06d8b510817a74cf42baba5b2f0808fcb2738e5f
ivaylo5ev <ivaylo5ev@gmail.com>
2020-12-18 14:07:11 +0200

    - doc comment improvements

--- codebase/data.versioning/main/src/Migrations/RequiresDataMigrationsAttribute.cs
+++ codebase/data.versioning/main/src/Migrations/RequiresDataMigrationsAttribute.cs
@@ -3,9 +3,15 @@ using Axle.Modularity;
 
 namespace Axle.Data.Versioning.Migrations
 {
+    /// <summary>
+    /// Causes the annotated target module to become dependent on the data migrator module.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
     public sealed class RequiresDataMigrationsAttribute : RequiresAttribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RequiresDataMigrationsAttribute"/> class.
+        /// </summary>
         public RequiresDataMigrationsAttribute() : base(typeof(MigratorModule)) { }
     }
 }
