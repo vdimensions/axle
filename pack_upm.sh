02aa1e91e1826d03dbe7ef38cc4b188d405b07cc
ivaylo5ev <ivaylo5ev@gmail.com>
2021-01-17 23:12:25 +0200

    - added upm packaging script

---
+++ pack_upm.sh
@@ -0,0 +1,24 @@
+dirs=(
+    "codebase/application/main"
+    "codebase/application/unity"
+    "codebase/caching/main"
+    "codebase/configuration/main"
+    "codebase/core/main"
+    "codebase/resources/main"
+    "codebase/resources/properties"
+    "codebase/resources/yaml"
+    "codebase/text.documents/main"
+    "codebase/text.documents/properties"
+    "codebase/text.documents/xml"
+    "codebase/text.documents/yaml"
+)
+base_dir=`pwd`
+version="1.10.1"
+registry="http://192.168.1.178:8373"
+
+for dir in ${dirs[@]}; do
+  cd "$base_dir/$dir"
+  npm version $version
+  npm publish --registry $registry
+  cd $base_dir
+done
